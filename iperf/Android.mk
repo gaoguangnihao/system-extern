45261cc5bdbab6f969fa754a147cbac559f9bc8f
dahui.cui <dahui.cui@kaiostech.com>
2019-07-17 17:39:39 +0800

    modify Android.mk

--- iperf/Android.mk
+++ iperf/Android.mk
@@ -4,39 +4,24 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 
-LOCAL_SRC_FILES := \
-    compat/delay.cpp \
-    compat/error.c \
-    compat/gettimeofday.c \
-    compat/inet_ntop.c \
-    compat/inet_pton.c \
-    compat/signal.c \
-    compat/snprintf.c \
-    compat/string.c \
-    compat/Thread.c
-
-
-LOCAL_SRC_FILES += \
-    src/Client.cpp \
-    src/Extractor.c \
-    src/gnu_getopt.c \
-    src/gnu_getopt_long.c \
-    src/Launch.cpp \
-    src/List.cpp \
-    src/Listener.cpp \
-    src/Locale.c \
-    src/main.cpp \
-    src/PerfSocket.cpp \
-    src/ReportCSV.c \
-    src/ReportDefault.c \
-    src/Reporter.c \
-    src/Server.cpp \
-    src/service.c \
-    src/Settings.cpp \
-    src/SocketAddr.c \
-    src/sockets.c \
-    src/stdio.c \
-    src/tcp_window_size.c
+LOCAL_SRC_FILES += src/cjson.c \
+    src/dscp.c \
+    src/iperf_api.c \
+    src/iperf_auth.c \
+    src/iperf_client_api.c \
+    src/iperf_error.c \
+    src/iperf_locale.c \
+    src/iperf_sctp.c \
+    src/iperf_server_api.c \
+    src/iperf_tcp.c \
+    src/iperf_time.c \
+    src/iperf_udp.c \
+    src/iperf_util.c \
+    src/main.c \
+    src/net.c \
+    src/tcp_info.c \
+    src/timer.c \
+    src/units.c 
 
 
 LOCAL_C_INCLUDES += \
