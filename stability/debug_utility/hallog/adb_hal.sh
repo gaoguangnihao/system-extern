021017719b31652c79b60229d6a2cfd8cc8086bd
dahui.cui <dahui.cui@kaiostech.com>
2019-07-29 16:43:49 +0800

    add RequestObserverProxy log

--- stability/debug_utility/hallog/adb_hal.sh
+++ stability/debug_utility/hallog/adb_hal.sh
@@ -4,9 +4,10 @@ adb root
 adb shell setenforce 0
 adb remount 
 adb shell setprop kaios.log.enable 1 
-#adb shell setprop kaios.log.filter hal:5,TCPSocket:5,TCPSocketc:5,TCPSocketp:5
+adb shell setprop kaios.log.filter hal:5,TCPSocket:5,TCPSocketc:5,TCPSocketp:5,nsRequestObserverProxy:5
 #adb shell setprop kaios.log.filter Layers:5,hal:5,fontlist:5,fontinit:5,textrun:5,gfx2d:5,textrunui:5,cmapdata:5,textperf:5,userfonts:5
-adb shell setprop kaios.log.filter Layers:5,hal:5,fontlist:5,fontinit:5,textrun:5,gfx2d:5,userfonts:5,frame:5
+#   nsRequestObserverProxy //nsRequestObserverProxy.cpp for 
+#adb shell setprop kaios.log.filter Layers:5,hal:5,fontlist:5,fontinit:5,textrun:5,gfx2d:5,userfonts:5,frame:5,nsRequestObserverProxy:5
 adb shell stop b2g 
 adb shell start b2g
 echo 'Done!'
