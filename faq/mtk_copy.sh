5b1c21f05ba7ce33cd484b563a28cc34c9edaece
dahui.cui <dahui.cui@kaiostech.com>
2019-09-17 13:26:51 +0800

    add mtk copy script

---
+++ faq/mtk_copy.sh
@@ -0,0 +1,19 @@
+#/bash
+echo 'begin copy'
+
+cp preloader_kaios31_256.bin /home/dhcui/dahui-share/mtk6731/
+cp lk-verified.bin /home/dhcui/dahui-share/mtk6731/
+cp loader_ext-verified.img /home/dhcui/dahui-share/mtk6731/
+cp md1rom-verified.img /home/dhcui/dahui-share/mtk6731/
+cp md1dsp-verified.img /home/dhcui/dahui-share/mtk6731/
+cp boot-verified.img /home/dhcui/dahui-share/mtk6731/
+cp recovery-verified.img /home/dhcui/dahui-share/mtk6731/
+cp spmfw-verified.bin /home/dhcui/dahui-share/mtk6731/
+cp mcupmfw-verified.img /home/dhcui/dahui-share/mtk6731/
+cp trustzone-verified.bin /home/dhcui/dahui-share/mtk6731/
+cp logo-verified.bin /home/dhcui/dahui-share/mtk6731/
+cp system.img /home/dhcui/dahui-share/mtk6731/
+cp userdata.img /home/dhcui/dahui-share/mtk6731/
+cp MT6731_Android_scatter.txt /home/dhcui/dahui-share/mtk6731/
+
+echo 'Done!'
