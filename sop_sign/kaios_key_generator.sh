733458e0e7aa25298aacf135ff5162c06a14e552
dahui.cui <dahui.cui@kaiostech.com>
2020-03-12 17:26:22 +0800

    update kaios_key_generator.sh

--- sop_sign/kaios_key_generator.sh
+++ sop_sign/kaios_key_generator.sh
@@ -1,31 +1,69 @@
+##########init valiable ###################
+#-e 判断$a是否存在
+#-d 判断$a是否存在，并且为目录
+#-f 判断$a是否存在，并且为常规文件
+#-L 判断$a是否存在，并且为符号链接
+#-h 判断$a是否存在，并且为软链接
+#-s 判断$a是否存在，并且长度不为0
+#-r 判断$a是否存在，并且可读
+#-w 判断$a是否存在，并且可写
+#-x 判断$a是否存在，并且可执行
+#-O 判断$a是否存在，并且属于当前用户
+#-G 判断$a是否存在，并且属于当前用户组
+#-nt 判断file1是否比file2新  [ "/usr/local/src/file1" -nt "/usr/local/src/file2" ]
+#-ot 判断file1是否比file2旧  [ "/usr/local/src/file1" -ot "/usr/local/src/file2" ]
+#########################################
+
+pd_Tools="../der_extractor/pem_to_der.py"
+de_Tools="../der_extractor/der_extractor"
+D_CURR="pwd"
+
+PRODUCT_NAME=$1
+
+if [ ! -d "$PRODUCT_NAME" ];then
+       mkdir $PRODUCT_NAME
+       echo "$PRODUCT_NAME Creat done"
+else 
+   
+  	echo $PRODUCT_NAME
+	exit
+fi
+
 # generator root key 
+echo '******generator keys and der begin******'
+
+cd $PRODUCT_NAME
+
 openssl genrsa -out root_prvk.pem 2048
 #
-python der_extractor/pem_to_der.py root_prvk.pem root_prvk.der
+python $pd_Tools root_prvk.pem root_prvk.der
 
 #
 openssl rsa -in root_prvk.pem -pubout >root_pubk.pem
 
 #
-python der_extractor/pem_to_der.py root_pubk.pem root_pubk.der
+python $pd_Tools root_pubk.pem root_pubk.der
 
 ##genator imgkey 
 
 openssl genrsa -out img_prvk.pem 2048
 #
-python der_extractor/pem_to_der.py img_prvk.pem img_prvk.der
+python $pd_Tools img_prvk.pem img_prvk.der
 
 #
 openssl rsa -in img_prvk.pem -pubout >img_pubk.pem
 
 #
-python der_extractor/pem_to_der.py img_pubk.pem img_pubk.der
+python $pd_Tools img_pubk.pem img_pubk.der
+
+#exit
+
+echo '******generator keys and der done******'
 
 
 echo 'use root_pubk.der generator oemkey.h and copy to matched folder (need release to odm)'
 
-
-#./der_extractor/der_extractor  root_pubk.der oemkey.h ANDROID_SBC
+$de_Tools  root_pubk.der oemkey.h ANDROID_SBC
 
 #copy oemkey.h to [DA] $DA_Kit/Raphael-da/custom/$PLATFORM/oemkey.h
 #copy oemkey.h to [PL] $PL/custom/$PORJECT/inc/oemkey.h
@@ -36,11 +74,21 @@ echo 'use root_pubk.der generator oemkey.h and copy to matched folder (need rele
 echo 'use root_pubk.der generator dakey.h this include DA_PL public key to verify preloader DA_PL.bin'
 #when sign DA_PL.bin use matched prvk.pem to sign
 
-#./der_extractor/der_extractor  root_pubk.der dakey.h ANDROID_SBC
+$de_Tools root_pubk.der dakey.h ANDROID_SBC
 
 #copy dakey.h to [PL]$PL/custom/$PROJECT/inc/dakey.h
 #/local/code/mtk-m/KaiOS/vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/kaios31_jpv/inc
 echo 'please note we need modify the dakey.h and replace keyword 'OEM' to 'DA' '
+
+cd ..
+
+## should be backup to server for 
+## contenct to server and copy to special folder temp to /local/keys-mtk
+
+DEST_FOLDER=/local/keys-mtk/
+
+cp -Rvdp $PRODUCT_NAME $DEST_FOLDER
+
 
 echo 'will be generator '
 
