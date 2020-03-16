b8686efbc137f3b71e8ec93bd5db8c327740eb2a
dahui.cui <dahui.cui@kaiostech.com>
2020-03-16 11:54:34 +0800

    add orign key about jpv

--- cert_generator/kaios_deploy_sign.sh
+++ cert_generator/kaios_deploy_sign.sh
@@ -33,6 +33,8 @@ echo '**********KAIOS-key-deploy Begin**************'
 
 bash ./sign-image_v2/img_key_deploy.sh
 
+#PLATFORM=mt6739 python ./sign-image_v2/img_key_deploy.py mt6739 kaios31_jpv cert1_key_path=/local/keys-mtk/kaios_orign/root_prvk.pem cert2_key_path=/local/keys-mtk/kaios_orign/img_prvk.pem root_key_padding=pss | tee img_key_deploy.log
+
 echo '***********KAIOS-key-deploy End*************'
 
 echo '**********KAIOS-CP-IMG Begin***********'
