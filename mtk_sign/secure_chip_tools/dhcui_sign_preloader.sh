da6f774cd231ca16645fb8fd640e9dcfdc462b8f
dahui.cui <dahui.cui@kaiostech.com>
2020-03-19 14:15:57 +0800

    update path about sign preloader

--- mtk_sign/secure_chip_tools/dhcui_sign_preloader.sh
+++ mtk_sign/secure_chip_tools/dhcui_sign_preloader.sh
@@ -23,14 +23,20 @@ function usage() {
 
 path_preloader=$1
 
+CURDIR="`pwd`"/"`dirname $0`"
+
+echo $CURDIR
 echo $path_preloader
 #if [ -f "$path_preloader/preloader_NO_GFH.bin" ]; then
 if [ -f "$path_preloader/preloader.bin" ]; then 
-   	cp $path_preloader/preloader.bin prebuilt/pbp/preloader.bin
+   	cp $path_preloader/preloader.bin $CURDIR/prebuilt/pbp/preloader.bin
 else
 	usage
 	exit
 fi
+
+
+#CURDIR="`pwd`"/"`dirname $0`"
 
 echo "preloader sign begin"
 
@@ -44,22 +50,22 @@ echo "preloader sign begin"
 ## below commond need root_prvk.pem to generator key_cert.bin 
 ## and sign the preloader
 
-python pbp.py -i settings/pbp/pl_key.ini -k out/pbp/key_cert.bin -g settings/pbp/pl_gfh_config_cert_chain.ini -c settings/pbp/pl_content.ini -func sign -o out/pbp/preloader-signed.bin prebuilt/pbp/preloader.bin
+python $CURDIR/pbp.py -i $CURDIR/settings/pbp/pl_key.ini -k $CURDIR/out/pbp/key_cert.bin -g $CURDIR/settings/pbp/pl_gfh_config_cert_chain.ini -c $CURDIR/settings/pbp/pl_content.ini -func sign -o $CURDIR/out/pbp/preloader-signed.bin $CURDIR/prebuilt/pbp/preloader.bin
 
 echo "use key_cert.bin to sign"
 
-python pbp.py -k out/pbp/key_cert.bin -g settings/pbp/pl_gfh_config_cert_chain.ini  -c settings/pbp/pl_content.ini -func sign -o out/pbp/preloader-signed.bin prebuilt/pbp/preloader.bin
+python $CURDIR/pbp.py -k $CURDIR/out/pbp/key_cert.bin -g $CURDIR/settings/pbp/pl_gfh_config_cert_chain.ini  -c $CURDIR/settings/pbp/pl_content.ini -func sign -o $CURDIR/out/pbp/preloader-signed.bin $CURDIR/prebuilt/pbp/preloader.bin
 
 #python resign_da.py prebuilt/resignda/MTK_AllInOne_DA.bin MT6739 settings/resignda/bbchips_pss.ini all out/resignda/MTK_AllInOne_DA.bin
 
-echo "preloader sign down"
+echo "preloader sign done"
 
 echo '============================================='
 echo '==copy sign preloader to the path_preloader=='
 echo '============================================='
 
 echo $path_preloader
-cp out/pbp/preloader-signed.bin $path_preloader/preloader_kaios31_jpv.bin
+cp $CURDIR/out/pbp/preloader-signed.bin $path_preloader/preloader_kaios31_jpv.bin
 
 echo success done 
 
